58b0a29f786eff800f7fb3c9fe16fa72b93e9712
Vikri Anto <30387870+Xeris459@users.noreply.github.com>
2023-05-25 13:25:01 +0800

    styling?

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
+const env = require('dotenv').config();
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("(TESTING CLOUD RUN) Halo Express!");
+app.get('/', (req, res) => {
+	res.send('(TESTING CLOUD RUN) Halo Express!');
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`App listening on port 5000`);
+app.listen(env.EXPRESS_PORT || 5000, () => {
+	console.log(`App listening on port ${env.EXPRESS_PORT || 5000}`);
 });
