16bf896fff891a55f938ceccf1d1be63de7e4b8e
Vikri Anto <30387870+Xeris459@users.noreply.github.com>
2023-05-31 11:21:56 +0800

    add forget password dan reset password controller di auth

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -1,7 +1,8 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, Prisma } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
+const { sendEmail } = require('../Lib/email');
 
 const prisma = new PrismaClient();
 
@@ -106,7 +107,7 @@ const getUser = async (req, res) => {
 				},
 			},
 		}).catch((err) => {
-			if (err instanceof prisma.NotFoundError) return res.status(404).json({ status: 'failed', code: 404, message: 'User not found' });
+			if (err instanceof Prisma.PrismaClientKnownRequestError) return res.status(404).json({ status: 'failed', code: 404, message: 'User not found' });
 
 			throw err;
 		});
@@ -170,4 +171,112 @@ const registerAttempt = async (req, res) => {
 	}
 };
 
-module.exports = { LoginAttempt, getUser, registerAttempt };
+const forgetPassword = async (req, res) => {
+	try {
+		const { email } = req.body;
+
+		const user = await prisma.user.findFirst({
+			where: {
+				email,
+			},
+		});
+
+		if (!user) {
+			return res.status(400).json({ status: 'failed', code: 400, message: 'Email not registered' });
+		}
+
+		const token = jwt.sign(
+			{
+				uuid: user.uuid,
+				email: user.email,
+			},
+			process.env.JWT_SECRET,
+			{ expiresIn: '1h' },
+		);
+
+		const message = `
+			<h1>Reset Password</h1>
+			<p>Click this link to reset your password</p>
+			<a href="${process.env.CLIENT_URL}/api/v1/reset-password/${token}">Reset Password</a>
+			`;
+
+		sendEmail(email, 'Reset Password', message);
+
+		prisma.userTokenResetPassword.create({
+			data: {
+				token,
+				uuid: uuidv4(),
+				expire: new Date(Date.now() + 3600000).toISOString().slice(0, 19).replace('T', ' '),
+				user: {
+					connect: {
+						uuid: user.uuid,
+					},
+				},
+			},
+		});
+
+		return res.status(200).json({
+			status: 'success',
+			code: 200,
+			message: 'Success Send Email',
+			token,
+		});
+	} catch (error) {
+		return res.status(500).json({ status: 'failed', code: 500, message: `Internal Server Error: ${error}` });
+	}
+};
+
+const resetPassword = async (req, res) => {
+	try {
+		const { password, passwordConfirm } = req.body;
+		const { token } = req.params;
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		if (password !== passwordConfirm) {
+			return res.status(400).json({ status: 'failed', code: 400, message: 'Password not match' });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const user = await prisma.user.update({
+			where: {
+				uuid: decoded.uuid,
+			},
+			data: {
+				password: hashedPassword,
+			},
+			select: {
+				uuid: true,
+				email: true,
+			},
+		});
+
+		await prisma.userTokenResetPassword.update({
+			where: {
+				token,
+			},
+			data: {
+				isUsed: true,
+			},
+		});
+
+		return res.status(200).json({
+			status: 'success',
+			code: 200,
+			message: 'Success Reset Password',
+			data: user,
+		});
+	} catch (error) {
+		return res.status(500).json({ status: 'failed', code: 500, message: `Internal Server Error: ${error}` });
+	}
+};
+
+module.exports = {
+	LoginAttempt,
+	getUser,
+	registerAttempt,
+	forgetPassword,
+	resetPassword,
+};
