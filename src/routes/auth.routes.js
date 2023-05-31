2f25a9668622bc28b5b76fdc6d54002b702083be
Vikri Anto <30387870+Xeris459@users.noreply.github.com>
2023-05-31 11:24:03 +0800

    add forget password dan reset password di router

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
 
-const { LoginAttempt, getUser, registerAttempt } = require('../Controllers/AuthController');
-const { verifyToken } = require('../Middlewares/userAuthorizationMiddleware');
+const {
+	LoginAttempt,
+	getUser,
+	registerAttempt,
+	resetPassword,
+	forgetPassword,
+} = require('../Controllers/AuthController');
+const { verifyToken, verifyValidTokenReset } = require('../Middlewares/userAuthorizationMiddleware');
 
 module.exports = (app) => {
 	router.post('/login', LoginAttempt);
 	router.post('/register', registerAttempt);
 
 	router.get('/user', verifyToken, getUser);
+
+	router.post('/forget-password', forgetPassword);
+	router.post('/reset-password/:token', verifyValidTokenReset, resetPassword);
 
 	app.use('/api/v1', router);
 };
