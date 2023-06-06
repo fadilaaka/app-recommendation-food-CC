e44e86522d820256c9e259ec5300a48d5e130cde
Vikri Anto <30387870+Xeris459@users.noreply.github.com>
2023-06-06 20:27:10 +0800

    update validasi

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,15 +8,22 @@ const {
 	forgetPassword,
 } = require('../Controllers/AuthController');
 const { verifyToken, verifyValidTokenReset } = require('../Middlewares/userAuthorizationMiddleware');
+const {
+	loginValidator,
+	registerValidator,
+	forgetPasswordValidator,
+	resetPasswordValidator,
+	validate,
+} = require('../Middlewares/validateMiddleware');
 
 module.exports = (app) => {
-	router.post('/login', LoginAttempt);
-	router.post('/register', registerAttempt);
+	router.post('/login', loginValidator, validate, LoginAttempt);
+	router.post('/register', registerValidator, validate, registerAttempt);
 
 	router.get('/user', verifyToken, getUser);
 
-	router.post('/forget-password', forgetPassword);
-	router.post('/reset-password/:token', verifyValidTokenReset, resetPassword);
+	router.post('/forget-password', forgetPasswordValidator, validate, forgetPassword);
+	router.post('/reset-password/:token', verifyValidTokenReset, resetPasswordValidator, validate, resetPassword);
 
 	app.use('/api/v1', router);
 };
