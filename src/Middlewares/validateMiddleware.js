4f1b88ce73dcd1ae373d739ff56d73d341b4bc64
Vikri Anto <30387870+Xeris459@users.noreply.github.com>
2023-06-10 13:28:18 +0800

    Update validateMiddleware.js

--- src/Middlewares/validateMiddleware.js
+++ src/Middlewares/validateMiddleware.js
@@ -1,4 +1,4 @@
-const { body, param, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 module.exports.validate = (req, res, next) => {
 	const errors = validationResult(req);
@@ -22,9 +22,12 @@ module.exports.registerValidator = [
 	body('name').isString(),
 	body('password').isString().withMessage('password must valid').notEmpty()
 		.isLength({ min: 8 })
+		.withMessage('password must contain at least 8 characters')
 		.isAlphanumeric()
 		.withMessage('password must contain at least 8 characters and alphanumeric'),
-	body('passwordConfirm').matches('password').withMessage('password did not match'),
+	body('passwordConfirm').exists({ checkFalsy: true }).withMessage('You must type a confirmation password')
+		.custom((value, { req }) => value === req.body.password)
+		.withMessage('The passwords do not match'),
 	body('email').isEmail().withMessage('email must valid').notEmpty(),
 ];
 
