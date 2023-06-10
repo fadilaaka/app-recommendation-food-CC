940892bf89e17c2a9daef59f820b5e637239db42
fadilaaka <akafadila09@gmail.com>
2023-06-10 17:50:05 +0700

    First Commit to deploy

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Use the official lightweight Python image.
+# https://hub.docker.com/_/python
+FROM python:3.10.7-slim-buster
+
+# Allow statements and log messages to immediately appear in the logs
+ENV PYTHONUNBUFFERED True
+
+# Copy local code to the container image.
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
+
+# Install production dependencies.
+RUN apt-get update
+RUN apt-get -y install default-libmysqlclient-dev
+RUN apt-get install libssl-dev -y
+RUN apt-get install python-dev default-libmysqlclient-dev -y
+RUN apt-get install python3-dev python3-wheel python-wheel python-pip python-dev build-essential -y
+RUN pip install --upgrade pip
+RUN pip install wheel setuptools --upgrade
+RUN pip install cmake
+RUN pip install mysqlclient
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
---
+++ app.py
@@ -0,0 +1,110 @@
+import os
+import pandas as pd
+import numpy as np
+import logging
+import fileapp
+import urllib
+from flask import Flask, request
+from flask_cors import CORS
+from tensorflow import keras
+from google.cloud import storage
+from fileobjects import DisplayInfo
+from fileobjects import UploadedFile
+
+app = Flask(__name__)
+CORS(app)
+
+project_id = 'aplikasi-rekomendasi-food'
+bucket_name = 'image-recogfavamn612211314'
+bucket_url = 'https://storage.googleapis.com/image-recogfavamn612211314'
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'aplikasi-rekomendasi-food-c5d232010e49.json'
+
+csv_path = './calorie bangkit.csv'
+dataset = pd.read_csv(csv_path)
+foods_sorted = sorted(os.listdir('./images'))
+
+model_best = keras.models.load_model(
+    './best_model_101class_101images.hdf5', compile=False)
+# model_best = keras.models.load_model('./food_c101_n10099_r64x64x3.h5')
+
+
+def predict_class(model, images):
+    results = []
+
+    for img_path in images:
+        img = keras.preprocessing.image.load_img(
+            img_path, target_size=(200, 200))
+        img = keras.preprocessing.image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = img / 255.
+
+        pred = model.predict(img)
+        index = np.argmax(pred)
+        pred_value = foods_sorted[index]
+
+        # Retrieve additional information from the dataset based on the predicted food
+        food_info = dataset[dataset['Food_Name'] == pred_value]
+        calorie = food_info['Calorie'].values[0]
+        protein = food_info['Protein'].values[0]
+        fat = food_info['Fat'].values[0]
+        carbohydrate = food_info['Carbohidrat'].values[0]
+
+        result = f"Calorie: {calorie}, Protein: {protein}, Fat: {fat}, Carbohidrat: {carbohydrate}"
+        results.append(result)
+
+    return results[0] if results else None
+
+
+@app.route('/')
+def hello():
+    image1 = ['./dodolgarut.jpg']
+    image2 = ['./miebakso.jpg']
+    image3 = ['./nasigoreng.jpg']
+    predict1 = predict_class(model_best, image1)
+    predict2 = predict_class(model_best, image2)
+    predict3 = predict_class(model_best, image3)
+    return {"data": [
+        {
+            "foto1": predict1
+        },
+        {
+            "foto2": predict2
+        },
+        {
+            "foto3": predict3
+        }
+    ]}
+
+
+@app.route('/upload-image-food', methods=['GET', 'POST'])
+def uploadImageFood():
+    if request.method == 'POST':
+        uploaded_file = request.files.get('file')
+
+        client = storage.Client(project_id)
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.blob(uploaded_file.filename)
+
+        try:
+            content = uploaded_file.content_type
+            blob.upload_from_string(uploaded_file.read(), content_type=content)
+
+            image = [f'{uploaded_file.filename}']
+            predict = predict_class(model_best, image)
+
+            return {"data":
+                    {
+                        "predict": predict,
+                        "imageUrl": f"{bucket_url}/{uploaded_file.filename}",
+                    },
+                    }
+
+        except Exception as e:
+            # Handle any exceptions that occur during the upload process
+            error_message = str(e)
+            return {"error": error_message}, 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
