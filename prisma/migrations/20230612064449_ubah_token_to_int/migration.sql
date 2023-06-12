5356539487f984bd1e8960ff33c8a146c026367c
Vikri Anto <30387870+Xeris459@users.noreply.github.com>
2023-06-12 15:14:03 +0800

    Update migration.sql

--- prisma/migrations/20230612064449_ubah_token_to_int/migration.sql
+++ prisma/migrations/20230612064449_ubah_token_to_int/migration.sql
@@ -1,8 +1,8 @@
 /*
-  Warnings:
+ Warnings:
+ - You are about to alter the column `token` on the `usertokenresetpassword` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+ */
 
-  - You are about to alter the column `token` on the `usertokenresetpassword` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
-
-*/
 -- AlterTable
-ALTER TABLE `usertokenresetpassword` MODIFY `token` INTEGER NOT NULL;
+
+ALTER TABLE `userTokenResetPassword` MODIFY `token` INTEGER NOT NULL;
